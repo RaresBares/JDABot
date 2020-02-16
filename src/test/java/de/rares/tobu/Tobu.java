303f62a0b90cd32a4902f0b445038ab4240a6d45
Rares <rsahleanu@gmail.com>
2020-02-16 21:50:55 +0100

    a

--- src/test/java/de/rares/tobu/Tobu.java
+++ src/test/java/de/rares/tobu/Tobu.java
@@ -4,19 +4,20 @@ import de.rares.tobu.listener.Listener;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.entities.TextChannel;
+import net.dv8tion.jda.api.sharding.DefaultShardManagerBuilder;
 
 import javax.security.auth.login.LoginException;
+import java.util.ArrayList;
 
 
 public class Tobu {
-
+    public static ArrayList<TextChannel> channels = new ArrayList<>();
     public static void main(String[] args) {
-        JDA jda;
+
         try {
-            jda = new JDABuilder("MzYyNjc0OTgzMjI0MjEzNTE0.Xixq7w.aOXsdykmGgqyF6nLJXvdI8SkcBY")
-                    .addEventListeners(new Listener())
-                    .setActivity(Activity.playing("moderation-Bot"))
-                    .build();
+           JDA jda = new JDABuilder("MzYyNjc0OTgzMjI0MjEzNTE0.XkmXHA.lX1JxM6CrAlFf75FB3Wi-mTf9us").setActivity(Activity.playing("a")).addEventListeners(new Listener()).build();
+
         } catch (LoginException e) {
             e.printStackTrace();
         }
--- src/test/java/de/rares/tobu/listener/Listener.java
+++ src/test/java/de/rares/tobu/listener/Listener.java
@@ -1,15 +1,17 @@
 package de.rares.tobu.listener;
 
+import de.rares.tobu.Tobu;
+import net.dv8tion.jda.api.MessageBuilder;
 import net.dv8tion.jda.api.Permission;
-import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.*;
 
-import net.dv8tion.jda.api.entities.Member;
-import net.dv8tion.jda.api.entities.TextChannel;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.requests.restaction.ChannelAction;
 
 import javax.annotation.Nonnull;
+import java.util.Collections;
+import java.util.UUID;
 
 public class Listener extends ListenerAdapter {
 
@@ -18,15 +20,58 @@ public class Listener extends ListenerAdapter {
         Guild g = e.getGuild();
         String msg = e.getMessage().getContentRaw();
         Member m = e.getMember();
+        TextChannel tc = e.getTextChannel();
 
-        if(msg.equalsIgnoreCase("/bewerben")){
-                    String name = m.getNickname();
-                     g.createTextChannel(name).setParent(g.getCategoriesByName("Bewerbungen",true).stream().findFirst().get()).queue();
-                    TextChannel c = g.getTextChannelsByName(name, false).stream().findFirst().get();
-            c.createPermissionOverride(m).deny(Permission.MESSAGE_WRITE).queue();
-            c.createPermissionOverride(m).deny(Permission.ALL_PERMISSIONS);
+        if (!Tobu.channels.contains(tc)) {
 
+
+            if (msg.equalsIgnoreCase("/bewerben")) {
+                String name = m.getNickname() == null ? "abc" : m.getNickname().toLowerCase();
+                e.getMessage().addReaction(":thumbsup:");
+
+                e.getMessage().delete().queue();
+
+                if (g.getTextChannelsByName("bewerbung-" + name, true).size() == 0) {
+
+                    g.createTextChannel("bewerbung-" + name).setParent(g.getCategoriesByName("bewerbungen", true)
+                            .get(0))
+                            .addPermissionOverride(m, null, Collections.singleton(Permission.MESSAGE_WRITE))
+                            .queue(c -> {
+                                Tobu.channels.add(c);
+
+                                for (Member member : g.getMembers()) {
+                                    if (!member.getRoles().contains(g.getRolesByName("Bewerbung", true))) {
+                                        if (member != c) {
+                                            try {
+                                                c.createPermissionOverride(member).deny(Permission.VIEW_CHANNEL).queue();
+
+                                            } catch (Exception ex) {
+
+                                            }
+
+                                        }
+
+                                    }
+                                }
+                                Message mes = new MessageBuilder()
+                                        .append("Lieber @" + m.getUser().getAsTag() + ",\n")
+                                        .append("Es wird sich ein Teamler um dich kümmern... Bitte habe etwas geduld!")
+                                        .build();
+                                c.sendMessage(mes).queue();
+
+
+                            });
+
+                } else {
+                    m.getUser().openPrivateChannel().queue(c -> c.sendMessage("Du hast bereits einen BewerbungsChannel geöffnet"));
+                }
+
+            }
+        }else{
+            if(msg.equalsIgnoreCase("quit")){
+                e.getMessage().addReaction(":thumbsup:");
+                tc.delete().queue();
+            }
         }
-
     }
 }
